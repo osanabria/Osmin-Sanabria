51713f4f425ecf690bbbf2e8b942f7df1cfca040
osanabria <you@example.com>
2020-06-15 12:30:06 -0500

    adding code

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var app = express();
+var bodyParser = require("body-parser");
+
+// APP CONFIG
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+
+//MAIN ROUTES
+app.get("/", function(req, res)
+{
+  if(res.err)
+  {
+      console.log("Error with /landing");
+  }
+  else
+  {
+          res.render("landing"); 
+  }
+});
+
+app.listen(3000, function(){
+  console.log("The Server Has Started!");
+});
